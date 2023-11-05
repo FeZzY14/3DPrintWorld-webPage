f294c6670a2d560067e07ef083debdd3b02bb9c9
FeZzY14 <jmisani123gmail.com>
2023-11-05 22:39:42 +0100

    vaiicko framework adaption

---
+++ App/Views/Home/about.view.php
@@ -0,0 +1,35 @@
+<div class="container item-listing">
+    <div class="row">
+        <div class="col">
+            <h3 class="about_heading">About Us</h3>
+
+            Welcome to 3D Print World,
+            your go-to destination for beautifully crafted 3D printed items.
+            We're more than just an online store; we're a passionate community of designers, creators,
+            and 3D printing enthusiasts who are dedicated to bringing innovation and creativity to your life.
+
+            <h3 class="about_heading">Our Story</h3>
+
+            Founded by a team of 3D printing aficionados, 3D Print World was born out of a shared love for the limitless possibilities
+            of 3D printing technology. We set out on a mission to make high-quality,
+            customizable 3D printed items accessible to everyone.
+
+            <h3 class="about_heading">Join Our Community</h3>
+
+            We're more than just a store; we're a community of 3D printing enthusiasts.
+            Connect with us on social media, and stay updated with the latest trends, tips, and tricks in the world of 3D printing.
+            We love seeing your creations, so don't forget to share your photos and tag us!
+
+            <h3 class="about_heading">Contact Us</h3>
+
+            Got a question or just want to say hello?
+            We'd love to hear from you. You can reach out to us through our email or social media,
+            and we'll get back to you as soon as possible.
+
+            Thank you for choosing 3D Print World as your destination for all things 3D printed.
+            We look forward to helping you discover and create the perfect items for your life.
+
+            Happy shopping!
+        </div>
+    </div>
+</div>
---
+++ App/Views/Home/index.view.php
@@ -0,0 +1,123 @@
+<div class="home-logo">
+    <img class="img-fluid logo_home" src="resources/logo.png" alt="logo image">
+</div>
+<div class="container">
+    <div class="row">
+        <div class="description">
+            <h1 class="home_welcome">Welcome to 3D Print World</h1>
+            Your Destination for Customized 3D Creations<br>
+            Are you ready to step into a world where imagination knows no bounds?<br>
+            Look no further than 3D Print Haven, your one-stop destination for all things 3D printed.<br>
+            We're here to bring your wildest ideas to life, one layer at a time.
+        </div>
+        <h2 class="item-type">Featured</h2>
+        <div class="col-md-4">
+            <div class="card-sl">
+                <a class="item-page-link" href="item.html">
+                    <img class="card-img-top" alt="product image" src="https://cdn.thingiverse.com/assets/ee/f4/73/74/4a/large_display_fd62e8c2-87bf-434e-816f-a787386851fd.png">
+                    <div class="card-body">
+                        <h3 class="card-title">cable holder</h3>
+                        <p class="card-text">
+                            this is simple
+                            cable holder that you can clamp
+                            on table or other things
+                        </p>
+                        <h3 class="card-prize">25,99$</h3>
+                    </div>
+                </a>
+                <a href="http://www.bing.com" class="card-button">add to cart</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-sl">
+                <a class="item-page-link" href="item.html">
+                    <img class="card-img-top" alt="product image" src="https://cdn.thingiverse.com/assets/ee/f4/73/74/4a/large_display_fd62e8c2-87bf-434e-816f-a787386851fd.png">
+                    <div class="card-body">
+                        <h3 class="card-title">cable holder</h3>
+                        <p class="card-text">
+                            this is simple
+                            cable holder that you can clamp
+                            on table or other things
+                        </p>
+                        <h3 class="card-prize">25,99$</h3>
+                    </div>
+                </a>
+                <a href="http://www.bing.com" class="card-button">add to cart</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-sl">
+                <a class="item-page-link" href="item.html">
+                    <img class="card-img-top" alt="product image" src="https://cdn.thingiverse.com/assets/ee/f4/73/74/4a/large_display_fd62e8c2-87bf-434e-816f-a787386851fd.png">
+                    <div class="card-body">
+                        <h3 class="card-title">cable holder</h3>
+                        <p class="card-text">
+                            this is simple
+                            cable holder that you can clamp
+                            on table or other things
+                        </p>
+                        <h3 class="card-prize">25,99$</h3>
+                    </div>
+                </a>
+                <a href="http://www.bing.com" class="card-button">add to cart</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <h2 class="item-type">Popular</h2>
+        <div class="col-md-4">
+            <div class="card-sl">
+                <a class="item-page-link" href="item.html">
+                    <img class="card-img-top" alt="product image" src="https://cdn.thingiverse.com/assets/ee/f4/73/74/4a/large_display_fd62e8c2-87bf-434e-816f-a787386851fd.png">
+                    <div class="card-body">
+                        <h3 class="card-title">cable holder</h3>
+                        <p class="card-text">
+                            this is simple
+                            cable holder that you can clamp
+                            on table or other things
+                        </p>
+                        <h3 class="card-prize">25,99$</h3>
+                    </div>
+                </a>
+                <a href="http://www.bing.com" class="card-button">add to cart</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-sl">
+                <a class="item-page-link" href="item.html">
+                    <img class="card-img-top" alt="product image" src="https://cdn.thingiverse.com/assets/ee/f4/73/74/4a/large_display_fd62e8c2-87bf-434e-816f-a787386851fd.png">
+                    <div class="card-body">
+                        <h3 class="card-title">cable holder</h3>
+                        <p class="card-text">
+                            this is simple
+                            cable holder that you can clamp
+                            on table or other things
+                        </p>
+                        <h3 class="card-prize">25,99$</h3>
+                    </div>
+                </a>
+                <a href="http://www.bing.com" class="card-button">add to cart</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card-sl">
+                <a class="item-page-link" href="item.html">
+                    <img class="card-img-top" alt="product image" src="https://cdn.thingiverse.com/assets/ee/f4/73/74/4a/large_display_fd62e8c2-87bf-434e-816f-a787386851fd.png">
+                    <div class="card-body">
+                        <h3 class="card-title">cable holder</h3>
+                        <p class="card-text">
+                            this is simple
+                            cable holder that you can clamp
+                            on table or other things
+                        </p>
+                        <h3 class="card-prize">25,99$</h3>
+                    </div>
+                </a>
+                <a href="http://www.bing.com" class="card-button">add to cart</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
